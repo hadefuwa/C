a2c84a1da508111c30267102739a8fabf9d0546f
Hamed Adefuwa <62181704+hadefuwa@users.noreply.github.com>
2020-07-11 17:19:39 +0100

    Add files via upload

---
+++ CProgrammingBookRitchieKernighan/farenheitv6.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// v6: For loop added to print next X number of temperatures as well
+
+main()
+{
+    float temp;
+
+start:
+    printf("\n\t\t Temperature Conversion Table\n\n");
+    printf("\n1.Fahrenheit To Celsius");
+    printf("\n2.Celsius To Fahrenheit");
+    printf("\n\n");
+    int option = 0;
+    scanf("%d", &option);
+
+    printf("Enter Temperature: ");
+    scanf("%f", &temp);
+
+    for (int loopCount = 0; loopCount < 5; loopCount++)
+    {
+
+        if (option == 1)
+        {
+            printf("\n%3.0f Fahrenheit = %6.1f Celcius\n", temp, (temp - 32.0) * (5.0 / 9.0));
+        }
+        else if (option == 2)
+        {
+            printf("\n%3.0f Celcius = %6.1f Fahrenheit\n", temp, (temp * (9.0 / 5.0) + 32));
+        }
+        temp++;
+    }
+
+    goto start;
+
+    return 0;
+}
