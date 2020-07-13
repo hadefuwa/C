5d1f39d5e855d736697c12bd4ac6287975933431
Hamed Adefuwa <62181704+hadefuwa@users.noreply.github.com>
2020-07-13 07:15:33 +0100

    Add files via upload

---
+++ CProgrammingBookRitchieKernighan/farenheitv7.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+// v6: For loop added to print next X number of temperatures as well
+// v7 define to remove magic numbers
+
+#define tempCoefficient 32.0
+#define fahtocelCalc 5.0 / 9.0
+#define celtofahCalc 9.0 / 5.0
+
+main()
+{
+    float temp;
+
+start:
+    printf("\n\t\t Temperature Conversion Table\n\n");
+    printf("\n1.Fahrenheit To Celsius");
+    printf("\n2.Celsius To Fahrenheit");
+    printf("\n\n");
+    int option = 0;
+    scanf("%d", &option);
+
+    printf("Enter Temperature: ");
+    scanf("%f", &temp);
+
+    for (int loopCount = 0; loopCount < 5; loopCount++)
+    {
+
+        if (option == 1)
+        {
+            printf("\n%3.0f Fahrenheit = %6.1f Celsius\n", temp, (temp - tempCoefficient) * (fahtocelCalc));
+        }
+        else if (option == 2)
+        {
+            printf("\n%3.0f Celsius = %6.1f Fahrenheit\n", temp, (temp * (celtofahCalc) + tempCoefficient));
+        }
+        temp++;
+    }
+
+    goto start;
+
+    return 0;
+}
