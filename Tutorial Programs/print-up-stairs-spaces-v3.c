a2f4e5317a238bfe6c2625459894ace7f5f66704
Hamed <h.adefuwa7558@student.leedsbeckett.ac.uk>
2020-07-20 22:00:49 +0100

    .

---
+++ Tutorial Programs/print-up-stairs-spaces-v3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+//print a downward stairs of blocks with spaces before each line 1 to N
+/*
+Like this;
+     *
+    **
+   ***
+  ****
+ *****
+******
+*/
+
+int main()
+{
+
+    int inputVar = 0;
+    printf("Enter a number: ");
+    scanf("%d", &inputVar);
+
+    for (int i = 1; i <= inputVar; i++)
+    {
+        for (int j = 1; j < i; j++)
+        {
+            printf(" ");
+        }
+        for (int k = i; k <= inputVar; k++)
+        {
+            printf("\u2588");
+        }
+        printf("\n");
+    }
+    return 0;
+}
