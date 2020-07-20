fef3e96c9acfb5af8b5a7452303a25efd5be7259
Hamed <h.adefuwa7558@student.leedsbeckett.ac.uk>
2020-07-20 18:27:22 +0100

    .

--- Tutorial Programs/print-up-stairs.c
+++ Tutorial Programs/print-up-stairs.c
@@ -8,7 +8,7 @@ int main()
     printf("Enter a number: ");
     scanf("%d", &inputVar);
 
-    for (int i = 1; i <= inputVar; i++)
+    for (int i = inputVar; i >= 0; i--)
     {
         for (int j = 1; j <= i; j++)
         {
