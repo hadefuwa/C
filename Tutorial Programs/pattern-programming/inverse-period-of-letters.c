19c52645b864b92d640b3773e80b40f74aabd2d9
Hamed <h.adefuwa7558@student.leedsbeckett.ac.uk>
2020-07-29 02:53:17 +0100

    .

---
+++ Tutorial Programs/pattern-programming/inverse-period-of-letters.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+//print a pyramid of spaces with letters around it
+/*
+Like this;
+**********
+****__****
+**______**
+*________*
+
+*/
+
+int main()
+{
+
+    int inputVar = 0;
+    printf("Enter a number: ");
+    scanf("%d", &inputVar);
+
+    for (int i = inputVar-1; i >= 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            printf("%c", 65+j);
+        }
+        for (int k = 1; k < (inputVar-i); k++)
+        {
+            printf(" ");
+        }        
+        for (int l = i; l >= 0; l--)
+        {
+            printf("%c", 65+l);
+        }
+        printf("\n");
+    }
+    return 0;
+}
