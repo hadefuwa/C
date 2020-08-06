f6e29bd86cd2d39a5753498d7763485a02447dbf
Hamed <h.adefuwa7558@student.leedsbeckett.ac.uk>
2020-08-06 18:50:36 +0100

    .

--- Tutorial Programs/Advance FOR Loop programs/47-factorial-number-loop.c
+++ Tutorial Programs/Advance FOR Loop programs/47-factorial-number-loop.c
@@ -5,11 +5,17 @@ int main()
 
     int i = 0;
     int n = 0;
-    int factorial = 0;
+    int factorial = 1;
 
-    printf("\nEnter a factorial:");
+    printf("\nEnter a number:");
     scanf("%d", &n);
 
+    for(i=1; i<=n; i++)
+    {
+        factorial=factorial*i;
+    }
+
+    printf("\nfactorial is %d\n", factorial);
 
 
     return 0;
